a74d036501deee56e3ffa1ab1d81db23f0db39ce
Srijith <srijithreddy.pingili@cerner.com>
2020-08-06 06:19:40 -0500

    splash screen

--- app/src/main/java/com/android/hackernewslite/play/ui/fragments/SplashScreenFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.android.hackernewslite.play.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.android.hackernewslite.play.R
-
-class SplashScreenFragment: Fragment(R.layout.fragment_splash_screen) {
-}
